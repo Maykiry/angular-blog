370eff3a9b4bda7c62a1345c612739a0c650f34f
Maykiry <mayconjmborges@gmail.com>
2024-02-13 19:41:21 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel anuncia novo homem de ferro",
+        "photoCover" : "https://images.entertainment.ie/uploads/2020/05/12140859/Iron-Man-2.jpg?w=640&h=384&q=high"
+    },
+    {
+        "id": "2",
+        "title": "Nova serie anunciada no dysney +",
+        "description": "bla bal blabla blba",
+        "photoCover" : "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    }
+]
