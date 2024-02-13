eee8b98a2afa15067677761629e846720252bd42
Maykiry <mayconjmborges@gmail.com>
2024-02-13 20:55:02 -0300

    content add

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,37 @@
 export const dataFake = [
     {
         "id": "1",
-        "title": "NOVO HOMEM DE FERRO EM 3D",
-        "description": "Marvel anuncia novo homem de ferro",
-        "photoCover" : "https://images.entertainment.ie/uploads/2020/05/12140859/Iron-Man-2.jpg?w=640&h=384&q=high"
+        "title": "Review: The Last of Us Part II Remastered faz o básico e mais um pouco",
+        "description": "Sem Volta.   O grande destaque de The Last of Us Part II Remastered é o modo Sem Volta, uma espécie de roguelike em que você avança por alguns desafios e, enquanto não morre, pode manter os itens que coletou e as vantagens que habilitou. Os desafios são gerados automaticamente, por padrão, mas existe um modo em que você pode personalizar o que pode entrar nesse esquema de aleatoriedades. Devo dizer que me surpreendi com esse modo. Não é algo realmente indispensável, e imagino que uma boa parte dos jogadores vá pular, mas é divertido para quem gosta do combate de The Last of Us — e eu estou nesse grupo. Desbloquear novos personagens cumprindo desafios, criar novos itens com materiais que encontramos nos cenários, enfrentar hordas de oponentes… É divertido por um tempo, mas não sei qual vai ser o limite para perfis diferentes de jogadores. Por mais que exista variação na concepção aleatória automática das fases, não há tanta diversidade de jogabilidade. Bastam algumas horas para ter visto tudo o que há para ser visto. O mais legal é poder controlar personagens que conhecemos no modo História, isso sim. Enquanto você tiver novos personagens para abrir, com certeza, irá se divertir. Contudo, depois que você tiver toda a lista desbloqueada, alternar entre eles vai ser legal no começo, mas, depois, talvez você não ligue mais. Por incrível que pareça, mesmo que o meu entusiasmo com esse aspecto do jogo seja limitado, o modo Sem Volta ainda é mais divertido e recompensador do que as chamadas Fases Perdidas, que são três fases cortadas do jogo final que a Naughty Dog disponibilizou com o remaster.",
+        "photoCover" : "https://makeindiegames.com.br/wp-content/uploads/2024/01/the-last-of-us-part-ii-remastered-pre-venda.jpg"
     },
     {
         "id": "2",
-        "title": "Nova serie anunciada no dysney +",
-        "description": "bla bal blabla blba",
-        "photoCover" : "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+        "title": "Final Fantasy 7 Rebirth: Veja antes e depois das cenas de Nibelheim",
+        "description": "A demo de Final Fantasy VII Rebirth, disponível para PS5, permite o teste de um trecho importante da história do jogo, no caso, parte dos acontecimentos das lembranças de Nibelheim e, com isso, fãs já podem comparar o novo game da Square Enix com sua versão original. O trecho da demo não vai além do flashback de Cloud em Nibelheim, quando o protagonista conta ao restante do grupo sobre o que aconteceu em sua vila natal.No caso, sem muitos spoilers, trata-se do momento em que Sephiroth deixa de ser um S.O.L.D.I.E.R. exemplar e se torna o icônico vilão da franquia com a popular cena em que se encontra num local rodeado de chamas.Com as cenas oferecidas na demonstração, fãs já puderam comparar o trecho com a versão original do game e checar a fidelidade do projeto, em relação título de PS1, lançado em 1997.",
+        "photoCover" : "https://i.ytimg.com/vi/DW34120lbZU/maxresdefault.jpg"
+    },
+
+    {
+        "id": "3",
+        "title": "Monster Hunter Rise chega a 14 milhões de cópias vendidas",
+        "description": "A Capcom atualizou sua página de jogos mais vendidos, junto da liberação dos relatórios financeiros da empresa relativos ao último trimestre de 2023. O grande destaque segue sendo Monster Hunter Rise; lançado em 2021, o jogo já acumula 14,2 milhões de unidades vendidas, um salto de 600 mil em relação ao fim de setembro. A liderança da lista ainda fica com Monster Hunter: World, com 19,6 milhões. Lançado em 2018, o jogo tem um total de 24,1 milhões em vendas ao se considerar a Iceborn Master Edition. Também vale destacar que, no último trimestre, o jogo teve um salto de 500 mil cópias vendidas, mostrando bastante longevidade. Os grandes títulos de 2023 da Capcom ainda não aparecem com grande destaque na lista. O remake de Resident Evil 4 tem 6,4 milhões de cópias vendidas e aparece em 13º. Street Fighter 6, escolhido como melhor jogo de luta do ano passado pelo The Game Awards, tem 2,9 milhões. Em seu relatório fiscal, a Capcom celebra as marcas e menciona Monster Hunter Wilds, novo jogo da série anunciado no The Game Awards, como um dos motivadores para as vendas consistentes de World e Rise.",
+        "photoCover" : "https://assets.nuuvem.com/image/upload/v1/products/615c9c21c2dcdc001d5c4e1b/sharing_images/nluuofxp6pnnk4mtn4v4.jpg"
+    },
+
+    {
+        "id": "4",
+        "title": "Assassin's Creed no Japão chega até março de 2025",
+        "description": "Assassin's Creed Codename Red, novo jogo da série que se passará no Japão feudal, deve ser lançado durante o próximo ano fiscal da Ubisoft, que acontece entre abril de 2024 e março de 2025. Como parte do relatório fiscal mais recente da companhia, o CEO Yves Guillemot comentou sobre os planos da empresa para o próximo ano, confirmando não só o lançamento de Codename Red para este período, mas também reforçando que Star Wars Outlaws chega até o fim de 2024. 'Seguindo em frente, estamos nos preparando para uma linha de produtos promissora para o ano fiscal de 2025, incluindo o lançamento de Star Wars Outlaws em 2024, que deve ser um grande marco na indústria de videogames, especialmente dado o interesse inegável desta franquia lendária que estará chegando pela primeira vez ao ambiente de Mundo Aberto, assim como Assassin's Creed Codename Red, situado no muito aguardado universo do Japão feudal', declarou Guillemot, que aparentemente acredita que o período histórico feudal do Japão é um universo próprio. Codename Red foi anunciado em setembro de 2022, e está em desenvolvimento pela Ubisoft Quebec, mesmo estúdio de Assassin's Creed Odyssey. Desde seu anúncio, pouco se ouviu falar do projeto, mas sabe-se que ele vai ser a iniciativa inicial de Assassin's Creed Infinity, nova plataforma que vai reunir os jogos da série em um único hub. Além de Codename Red, outro jogo com temática similar que sai em breve é Rise of the Ronin, com data para 22 de março no PS5. Já Star Wars Outlaws foi revelado no showcase de Xbox de junho de 2023, e está sendo desenvolvido pelo estúdio Massive, de The Division. O jogo tem previsão para 2024, mas sem data específica. Uma publicação do blog oficial dos parques da Disney indicou que ele seria lançado no fim de 2024, mas a Ubisoft declarou mais tarde que teria sido um erro de comunicação.",
+        "photoCover" : "https://meups.com.br/wp-content/uploads/2023/07/Assassins-Creed-Codename-Red.jpg"
+    },
+
+    {
+        "id": "5",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Marvel anuncia novo homem de ferro",
+        "photoCover" : "https://images.entertainment.ie/uploads/2020/05/12140859/Iron-Man-2.jpg?w=640&h=384&q=high"
     }
+
+
 ]
